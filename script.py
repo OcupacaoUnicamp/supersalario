16d9839286090ff02ee9c0deab6dc26fdc620ac1
OcupacaoUnicamp <ocupaunicamp@openmailbox.org>
2016-05-13 06:48:42 -0300

    Add files via upload

--- script.py
+++ script.py
@@ -9,13 +9,16 @@ with open('../salarios.csv', 'rb') as csvfile:
 	
 	# Traduz os dados da planilha para o dicionário
 	salarios = {}
-	print "Pessoas que possuem dupla matrícula:"
 	for row in planilha: 
-		if row[0] in salarios:
-			print row[0]			
+		if row[0] in salarios:			
 			salarios[row[0]] += float(row[1])
 		else:
 			salarios[row[0]] = float(row[1])
+
+	# Total salários (qualquer valor)
+	total_salarios = 0
+	for nome in salarios: 
+		total_salarios += salarios[nome] 
 
 	#  Separa super salários (acima de R$21.613,05)
 	supersalarios = {}
@@ -31,4 +34,6 @@ with open('../salarios.csv', 'rb') as csvfile:
 	# Relatório
 	print "Funcionários na folha de pagamento: %d" % len(salarios)
 	print "Funcionários com super-salários (acima de R$21.613,05): %d (%.1f%%)" % (float(len(supersalarios)), float(len(supersalarios))/float(len(salarios))*100)
+	print "Total dos salários: R$%d/mês | R$%.2f/ano   " % (total_salarios,total_salarios*12) 
 	print "Excedente dos super-salários: R$%.2f/mês | R$%.2f/ano" % (excedente_supersalarios, excedente_supersalarios*12)
+
